84f64127547466b607e47c2c6cc651f7ee36c4f0
Tafitaniaina-GG <155243500+Tafitaniaina-GG@users.noreply.github.com>
2024-10-20 18:41:10 +0300

    Update smsBomb.js

--- commands/smsBomb.js
+++ commands/smsBomb.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = {
-  name: 'gpt4o',
+  name: 'Ai',
   description: 'Pose une question à plusieurs services AI et obtient la réponse la plus rapide.',
   author: 'ArYAN',
   async execute(senderId, args, pageAccessToken, sendMessage) {
