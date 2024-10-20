10582c524375700a2be50555d70792a742c2e5da
Tafitaniaina-GG <155243500+Tafitaniaina-GG@users.noreply.github.com>
2024-10-20 13:54:59 +0300

    Update gemini.js

--- commands/gemini.js
+++ commands/gemini.js
@@ -1,7 +1,7 @@
 const { callGeminiAPI } = require('../utils/callGeminiAPI');
 
 module.exports = {
-  name: 'g',
+  name: 'gemini',
   description: '📩 Utiliser Gemini',
   author: 'ChatGPT',
   async execute(senderId, args, pageAccessToken, sendMessage) {
