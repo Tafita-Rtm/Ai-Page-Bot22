2b3fcd9a6bfc23b307853d4dab00baa1737989fc
Tafitaniaina-GG <155243500+Tafitaniaina-GG@users.noreply.github.com>
2024-10-20 17:51:06 +0300

    Update claude3.js

--- commands/claude3.js
+++ commands/claude3.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const path = require('path');
 
 module.exports = {
-  name: 'help22',
+  name: 'claude3',
   description: 'Posez une question à GPT-4o ou répondez avec une image.',
   author: 'Deku (rest api)',
   async execute(senderId, args, pageAccessToken, sendMessage) {
